c94501ba1b2447c64fb1a5491b5d8ef5dfa248e5
Maithri Rao <maithrirao30@gmail.com>
2025-07-13 20:14:55 +0200

    Implement RAG chatbot with LangChain and ChromaDB

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Python shell flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    lib = nixpkgs.lib;
+    forAllSystems = function: lib.genAttrs [
+      "aarch64-linux"
+      "x86_64-linux"
+    ] function;
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+        jupyterlab
+        pandas
+      ]);
+    in {
+      default = pkgs.mkShellNoCC {
+        packages = [
+          pythonEnv
+        ];
+
+        shellHook = ''
+          export PYTHONPATH="${lib.getExe pythonEnv}"
+        '';
+      };
+    });
+  };
+}
